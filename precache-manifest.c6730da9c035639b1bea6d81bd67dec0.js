3bbfc35115e8208a9c48f0abce6aa3429400db1d
Kenneth Ossai <kenossai@ymail.com>
2020-05-27 00:33:52 +0100

    Updates

---
+++ precache-manifest.c6730da9c035639b1bea6d81bd67dec0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51b9b98b7bf8552f27790caa4ca6fd05",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "cc12151989f29244926f",
+    "url": "/movie-app/static/css/main.e9788ed4.chunk.css"
+  },
+  {
+    "revision": "dae0632ef8b725036ffa",
+    "url": "/movie-app/static/js/2.3b110aa8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-app/static/js/2.3b110aa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc12151989f29244926f",
+    "url": "/movie-app/static/js/main.19613b6c.chunk.js"
+  },
+  {
+    "revision": "255bee9b5e55438d0d88",
+    "url": "/movie-app/static/js/runtime-main.4f650522.js"
+  }
+]);
